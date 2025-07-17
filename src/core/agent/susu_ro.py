b030d9196be7540513656c2a9b3e1a55e8613b41
maximofn <maximofn@gmail.com>
2025-07-17 19:00:19 +0200

    Add environment variables and the dotenv library for configuration management. Update the .env.demo file with new keys and modify pyproject.toml to include dotenv as a dependency. Improve the functionality of the SusuRo agent to use environment variables in its operation.

--- src/core/agent/susu_ro.py
+++ src/core/agent/susu_ro.py
@@ -6,10 +6,26 @@ from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
  
 from langchain_ollama.chat_models import ChatOllama
-
 from langchain_core.messages import HumanMessage
 
+from langsmith import traceable
+
 from core.stt.whisper import Whisper
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Load the environment variables
+LANGSMITH_API_KEY = os.getenv("LANGSMITH_API_KEY")
+LANGSMITH_ENDPOINT = os.getenv("LANGSMITH_ENDPOINT")
+LANGSMITH_PROJECT = os.getenv("LANGSMITH_PROJECT")
+
+os.environ["LANGSMITH_TRACING_V2"] = "true"
+os.environ["LANGSMITH_API_KEY"] = LANGSMITH_API_KEY
+os.environ["LANGSMITH_ENDPOINT"] = LANGSMITH_ENDPOINT
+os.environ["LANGSMITH_PROJECT"] = LANGSMITH_PROJECT
 
 # State
 class State(TypedDict):
@@ -55,11 +71,11 @@ class SusuRo():
             "chatbot_node",
             tools_condition,
             {
-                "stt_tool": "stt_tool",
+                "tools": "stt_tool",  # Cuando hay tool calls, va a stt_tool
+                "__end__": END,       # Cuando no hay tool calls, termina
             }
         )
         self.graph_builder.add_edge("stt_tool", "chatbot_node")
-        self.graph_builder.add_edge("chatbot_node", END)
 
         # Compile the graph
         self.graph = self.graph_builder.compile()
@@ -70,9 +86,18 @@ class SusuRo():
         except Exception as e:
             print(f"Error al visualizar el grafo: {e}")
     
-    # Function
+    # Functions
+    @traceable(run_type="llm")
     def chatbot_function(self, state: State):
+        input_messages = state["messages"]
+        response = self.llm_with_tools.invoke(input_messages)
+        print(input_messages)
+        print(response)
         return {"messages": [self.llm_with_tools.invoke(state["messages"])]}
+    
+    @traceable(run_type="tool")
+    def stt_function(self, state: State):
+        return {"messages": [self.stt.transcribe(state["messages"])]}
     
     # Invoke 
     def invoke(self, prompt: str):
--- src/main.py
+++ src/main.py
@@ -2,20 +2,15 @@ from core.agent.susu_ro import SusuRo
 from core.stt.whisper import Whisper
 
 def main():
-    prompt = "Hola, cómo estás?"
+    prompt = "Transcribe el audio data/MicroMachines.mp3"
 
     susu = SusuRo()
 
-    # messages = susu.invoke(prompt)
+    messages = susu.invoke(prompt)
     
-    # for message in messages:
-    #     message.pretty_print()
+    for message in messages:
+        message.pretty_print()
 
-    # stt = Whisper()
-    # audio_path = "data/MicroMachines.mp3"
-    # result = stt.transcribe(audio_path)
-    # print(result)
-    
 
 if __name__ == "__main__":
     main()
